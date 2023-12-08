75a610a5b47ec1ebe5a5a9b009ed81ee58a1b08d
oussamaza25 <oussama.zahdi25@gmail.com>
2023-12-08 15:16:30 +0100

    last

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ozahdi <ozahdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 09:42:23 by ozahdi            #+#    #+#             */
-/*   Updated: 2023/12/01 13:32:30 by ozahdi           ###   ########.fr       */
+/*   Updated: 2023/12/08 15:15:34 by ozahdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if ((int)count < 0 && (int)size < 0)
+	if (count && size && ((count *size) / size != count))
 		return (NULL);
 	ptr = malloc(count * size);
 	if (ptr == NULL)
